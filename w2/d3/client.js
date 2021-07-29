aea770aee92dbb1b45f090612dd71bb61b4be799
Sonam <sonam.malhotra2496@gmail.com>
2021-07-29 18:37:54 +0000

    i made connection so It can  send msg to the server

---
+++ w2/d3/client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const connection = function(){
+  const conn = net.createConnection({ 
+  host: '135.23.223.133', // change to IP address of computer or ngrok host if tunneling
+  port: 50542 // or change to the ngrok port if tunneling
+  });
+
+
+  conn.setEncoding('utf8'); // interpret data as text
+  conn.on('data', (data) => {
+  console.log('Message from client: ', data)
+  });
+  conn.on('connect', () => {
+  conn.write('Name: SM24');
+  conn.write('Move: up');
+  conn.write('Move: right');
+  });
+
+
+  return conn;
+}
+module.exports = connection;
+
