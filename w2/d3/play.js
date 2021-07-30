242986103d555b7321be924d20da67bd42219bae
Sonam <sonam.malhotra2496@gmail.com>
2021-07-30 00:17:40 +0000

    ctrl +c handling user input

--- w2/d3/play.js
+++ w2/d3/play.js
@@ -1,19 +1,26 @@
-const net = require('net');
-const connection = function(){
-const conn = net.createConnection({ 
+const connection = require("./client");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  connection();
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+   
+
+  }
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+
+};
 
 
-  host: '135.23.223.133', // change to IP address of computer or ngrok host if tunneling
-  port: 50542 // or change to the ngrok port if tunneling
-});
+setupInput();
 
 
-conn.setEncoding('utf8'); // interpret data as text
-conn.on('data', (data) => {
-  console.log('Message from client: ', data)
-});
-conn.on('connect', () => {
-  conn.write('Name: SM');
-});
-return conn;
-}
+
+
